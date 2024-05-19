7932b34999707c1094cf4cf1531c01e05f551b2d
ricardo lemos <l_riki@hotmail.com>
2024-05-19 19:28:13 -0300

    Json productos + rutas

--- 1ra PreEntrega/src/app.js
+++ 1ra PreEntrega/src/app.js
@@ -1,13 +1,18 @@
 import express from "express";
-// import routes from "./routes/index.js";
+import rutas from "./routes/index.routes.js";
+// import __dirname from "./dirname.js"
 
+const PORT = 8080;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/static",express.static("public"));
 
-// app.use("/api", routes);
+//mis Rutas
+app.use("/api", rutas);
 
-app.listen(8080, () => {
-  console.log("El servidor ahora esta escuchando en el puerto 8080");
+
+app.listen(PORT, () => {
+  console.log(`El servidor ahora esta escuchando en el puerto ${PORT}`);
 });
---
+++ 1ra PreEntrega/src/routes/productos.routes.js
@@ -0,0 +1,9 @@
+import {Router} from "express";
+// import productManager from "../productManager.js";
+// import {checkProductData} from "../middlewares/checkProductData.middleware.js";
+
+const router = Router();
+
+
+
+export default router;
