8ecfc4af886024a5a20e345fe1a5fde2dc7475be
ricardo lemos <l_riki@hotmail.com>
2024-06-02 20:22:48 -0300

    Copio entrega1 y agrego archivos de vistas

---
+++ 2da PreEntrega/src/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import rutas from "./routes/index.routes.js";
+import __dirname from "./dirname.js"
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import viewsRoutes from "./routes/views.routes.js";
+import productManager from "./productManager.js";
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/static",express.static("public"));
+
+//2da entrega
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
+
+//mis Rutas
+app.use("/api", rutas);
+
+
+//mis Vistas
+app.use("/", viewsRoutes);
+
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`El servidor ahora esta escuchando en el puerto ${PORT}.`);
+});
+
+//configuro el Socket
+export const io = new Server(httpServer);
+
+io.on("connection", async socket => {
+  console.log("Un nuevo usuario se ha conectado al servidor.");
+  // const products = await productManager.getProducts();
+  // io.emit("products", products );
+});
---
+++ 2da PreEntrega/src/middlewares/verif_InfoProducto.middleware.js
@@ -0,0 +1,30 @@
+import { request, response } from "express";
+import productManager from "../productManager.js";
+
+
+export const verif_InfoProducto = async (req = request, res = response, next) => {
+    try {
+        const { title, description, price, code, stock, category } = req.body;
+        const newProduct = {
+            title,
+            description,
+            price,
+            code,
+            stock,
+            category
+        };
+
+        const products = await productManager.getProducts();
+        const productExists = products.find((p) => p.code === code);
+        if (productExists) return res.status(400).json({ status: "Error", msg: `El producto con el código ${code} ya se ha agregado.` });
+
+        const checkData = Object.values(newProduct).includes(undefined);
+        if (checkData) return res.status(400).json({ status: "Error", msg: "Todos los datos del producto son de ingreso obligatorio." });
+
+        next(); // continuo a la ejecución de mi endpoint
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: "Error", msg: "Ha ocurrido un error interno del servidor." });
+    }
+};
